171ba8dc005e7f250ac0893e9d1285a30445e491
Jennifer Maia <jen_maia@hotmail.com>
2025-06-16 18:39:17 -0300

    Adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -13,11 +13,13 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-num_01 = int(input("Insira um número inteiro: "))
-num_02 = int(input("Insira outro número inteiro: "))
 
-resultado = (num_01 // num_02)
-print (resultado)
+        #num_01 = int(input("Insira um número inteiro: "))
+        #num_02 = int(input("Insira outro número inteiro: "))
+
+        #resultado = (num_01 // num_02)
+        #print (resultado)
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
@@ -27,7 +29,11 @@ print (resultado)
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math
 
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+print(f"{area_do_circulo:.2f}")
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
